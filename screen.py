98dfbda84c4466a6d8fe5e3c545230567807de97
KayOrNoffil <noffil1@yandex.ru>
2025-02-21 15:24:54 +0300

    scale map

--- screen.py
+++ screen.py
@@ -1,25 +1,23 @@
-import sys
-
 import pygame
-import requests
+import utils as ut
 
 server_address = 'https://static-maps.yandex.ru/v1?'
 api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
 
 spn = '&spn='
-coordinates = (29.975970, 31.130784)
-current_spn = 0.003
+coordinates = [29.975970, 31.130784]
+current_spn = [0.003, 0.003]
+factor = 1
 
 # Инициализируем pygame
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
-n = 0
 
 clock = pygame.time.Clock()
 running = True
 fps = 60
 
-current_image = pygame.surface.Surface(screen.get_size())
+current_image = ut.get_image(coordinates, current_spn)
 
 
 while running:
@@ -28,24 +26,14 @@ while running:
             running = False
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                print(pygame.K_UP)
-            n += 1
-        else:
-            map_request = f"{server_address}ll={coordinates[1]},{coordinates[0]}&spn={current_spn},{current_spn}&apikey={api_key}"
-            response = requests.get(map_request)
-
-            if not response:
-                print("Ошибка выполнения запроса:")
-                print(map_request)
-                print("Http статус:", response.status_code, "(", response.reason, ")")
-                sys.exit(1)
-
-            # Запишем полученное изображение в файл.
-            map_file = "map.png"
-            with open(map_file, "wb") as file:
-                file.write(response.content)
-            current_image = pygame.image.load(map_file)
-            print(current_image.__class__)
+                current_spn[0] += 2 ** factor * 0.0005
+                current_spn[1] += 2 ** factor * 0.0005
+                factor += 1
+            if event.key == pygame.K_DOWN:
+                current_spn[0] -= 2 ** factor * 0.0005
+                current_spn[1] -= 2 ** factor * 0.0005
+                factor -= 1
+            current_image = ut.get_image(coordinates, current_spn)
     screen.blit(current_image, (0, 0))
     pygame.display.flip()
     clock.tick(fps)
